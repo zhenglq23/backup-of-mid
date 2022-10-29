14b3ffe2297490c0f6a19f21f8f4dc8390154a08
user <user@user.local>
2022-10-29 12:40:53 +0800

    part4 3

---
+++ GridWorldCode/part4/ChameleonKid.java
@@ -0,0 +1,65 @@
+/* 
+ * AP(r) Computer Science GridWorld Case Study:
+ * Copyright(c) 2005-2006 Cay S. Horstmann (http://horstmann.com)
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @author Chris Nevison
+ * @author Barbara Cloud Wells
+ * @author Cay Horstmann
+ */
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
+import java.util.ArrayList;
+import java.awt.Color;
+
+/**
+ * A <code>ChameleonCritter</code> takes on the color of neighboring actors as
+ * it moves through the grid. <br />
+ * The implementation of this class is testable on the AP CS A and AB exams.
+ */
+public class ChameleonKid extends ModifiedChameleonCritter
+{
+    /**
+     * get Actors that are in front or behind.
+     */
+    public ArrayList<Actor> getActors()
+    {
+        ArrayList<Actor> fb = new ArrayList<Actor>();
+        int[] dirs = {Location.AHEAD,Location.AHEAD+180};
+        for (Location loc : getLocationsInDirections(dirs))
+        {
+            Actor a = getGrid().get(loc);
+            if (a != null)
+                fb.add(a);
+        }
+        return fb;
+    }
+    
+    public ArrayList<Location> getLocationsInDirections(int[] directions)
+    {
+        ArrayList<Location> locs = new ArrayList<Location>();
+        Grid gr = getGrid();
+        Location loc = getLocation();
+    
+        for (int d : directions)
+        {
+            Location neighborLoc = loc.getAdjacentLocation(getDirection() + d);
+            if (gr.isValid(neighborLoc))
+                locs.add(neighborLoc);
+        }
+        return locs;
+    }    
+
+}
